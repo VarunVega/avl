e9cb6d5bae83626e4ee561b3ce27f2f775f1a9dc
VarunVega <54569645+VarunVega@users.noreply.github.com>
2019-08-30 18:23:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -568,7 +568,115 @@ export default class AVLTree {
     return newNode;
   }
 
+/**
+   * Removes the node from the tree. If not found, returns null.
+   * @param  {Key} key
+   * @return {?Node}
+   */
+  removeMatchingEngine (key,order_id) {
+    if (!this._root) return null;
 
+    var node = this._root;
+    var compare = this._comparator;
+    var cmp = 0;
+
+    while (node) {
+      cmp = compare(key, node.key);
+      if((cmp === 0)&&order_id==node.data.order ) break;
+
+      //L or R biased
+      else if (cmp > 0)   node = node.right;
+      else    node = node.left;
+    }
+    if (!node) return null;
+
+    var returnValue = node.key;
+    var max, min;
+
+    if (node.left) {
+      max = node.left;
+
+      while (max.left || max.right) {
+        while (max.right) max = max.right;
+
+        node.key = max.key;
+        node.data = max.data;
+        if (max.left) {
+          node = max;
+          max = max.left;
+        }
+      }
+
+      node.key  = max.key;
+      node.data = max.data;
+      node = max;
+    }
+
+    if (node.right) {
+      min = node.right;
+
+      while (min.left || min.right) {
+        while (min.left) min = min.left;
+
+        node.key  = min.key;
+        node.data = min.data;
+        if (min.right) {
+          node = min;
+          min = min.right;
+        }
+      }
+
+      node.key  = min.key;
+      node.data = min.data;
+      node = min;
+    }
+
+    var parent = node.parent;
+    var pp     = node;
+    var newRoot;
+
+    while (parent) {
+      if (parent.left === pp) parent.balanceFactor -= 1;
+      else                    parent.balanceFactor += 1;
+
+      if        (parent.balanceFactor < -1) {
+        // inlined
+        //var newRoot = rightBalance(parent);
+        if (parent.right.balanceFactor === 1) rotateRight(parent.right);
+        newRoot = rotateLeft(parent);
+
+        if (parent === this._root) this._root = newRoot;
+        parent = newRoot;
+      } else if (parent.balanceFactor > 1) {
+        // inlined
+        // var newRoot = leftBalance(parent);
+        if (parent.left.balanceFactor === -1) rotateLeft(parent.left);
+        newRoot = rotateRight(parent);
+
+        if (parent === this._root) this._root = newRoot;
+        parent = newRoot;
+      }
+
+      if (parent.balanceFactor === -1 || parent.balanceFactor === 1) break;
+
+      pp     = parent;
+      parent = parent.parent;
+    }
+
+    if (node.parent) {
+      if (node.parent.left === node) node.parent.left  = null;
+      else                           node.parent.right = null;
+    }
+
+    if (node === this._root) this._root = null;
+
+    this._size--;
+    return returnValue;
+  }
+
+  
+  
+  
   /**
    * Removes the node from the tree. If not found, returns null.
    * @param  {Key} key
